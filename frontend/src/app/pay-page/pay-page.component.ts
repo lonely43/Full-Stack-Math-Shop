81600d85490a407af2add7e08792786ba63f217c
lonely43 <iladenisov327@gmail.com>
2023-05-03 16:30:17 +0300

    add pay-page

--- frontend/src/app/pay-page/pay-page.component.ts
+++ frontend/src/app/pay-page/pay-page.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { ItemsDTO } from '../list-items/item.model';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-pay-page',
   templateUrl: './pay-page.component.html',
   styleUrls: ['./pay-page.component.scss']
 })
-export class PayPageComponent {
+export class PayPageComponent implements AfterViewInit {
+  constructor(private appComponent: AppComponent){
+  }
 
+  items: ItemsDTO[] = this.appComponent.items
+
+  ngAfterViewInit(): void {
+    this.updateItems()
+  }
+
+  updateItems(){
+    this.items = this.appComponent.items
+  }
+  addOne(id: number){
+    return this.appComponent.addToCartOne(id)
+  }
+  minOne(id: number){
+    return this.appComponent.minToCartOne(id)
+  }
+  removeItem(id: number, element){
+    return this.appComponent.removeFromCart(id, element)
+  }
 }
